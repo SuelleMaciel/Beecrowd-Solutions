03759fedd49463361dd492329742555e97a6d31b
Suelle <suellemaciel7@gmail.com>
2024-10-08 18:06:36 -0300

    "A biblioteca do Senhor Severino"

---
+++ 2137.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int biblioteca[1000];
+    int n;
+
+    while (scanf("%d", &n) != EOF) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &biblioteca[i]);
+        }
+
+        qsort(biblioteca, n, sizeof(int), compare);
+
+        for (int i = 0; i < n; i++) {
+            printf("%04d\n", biblioteca[i]);
+        }
+    }
+
+    return 0;
+}
